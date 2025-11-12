43f54e184a9bd7cc66e2fe98cf95522a82805250
GersonVala <gersonvalashek@gmail.com>
2025-11-12 08:01:42 -0300

    Spring inicializado

---
+++ src/main/java/com/GestionReservasDeVehiculos/RentaDeVehiculos/RentaDeVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.GestionReservasDeVehiculos.RentaDeVehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaDeVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaDeVehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GestionReservasDeVehiculos/RentaDeVehiculos/RentaDeVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionReservasDeVehiculos.RentaDeVehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaDeVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
